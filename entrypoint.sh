140119605cc75149a4e59285f54c6f36769e7fdf
jpsuzuki <kntr0437jc@gmail.com>
2021-12-23 10:57:43 +0900

    環境変数など修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
-
-# Remove a potentially pre-existing server.pid for Rails.
-
 rm -f /test_app/tmp/pids/unicorn.pid
+# Remove a potentially pre-existing server.pid for Rails.
+if [ "${RAILS_ENV}" = "production" ]
+then
+  bundle exec rails assets:precompile
+fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
